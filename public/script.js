65930074b63bd70b848f10e43b7e3b1398e0f85c
David Ban <djban@berkeley.edu>
2023-07-26 15:05:42 -0700

    cleaned up commented code

--- public/script.js
+++ public/script.js
@@ -103,52 +103,6 @@ async function stopRecording() {
   formData.append('audio', new Blob(audioChunks.map(chunk => chunk.data), { type: 'audio/webm' }), 'recorded_audio.webm');
 
 
-  // try {
-  //   // Send the recorded audio data to the server using fetch and a POST request
-  //   const response = await fetch('/upload', {
-  //     method: 'POST',
-  //     body: formData
-  //   });
-
-  //   if (response.ok) {
-  //     console.log('Audio file uploaded successfully.');
-  //     console.log(formData);
-      
-
-  //     // const data = await response.json();
-  //     const downloadURL = '/recorded_audio/recorded_audio.webm';
-
-
-  //     // Trigger the server-side download by making another fetch request
-  //     const downloadResponse = await fetch(downloadURL);
-  //     if (downloadResponse.ok) {
-  //       console.log('Audio file downloaded successfully on the server.');
-  //     } else {
-  //       console.error('Error downloading audio file on the server:', downloadResponse.statusText);
-  //     }
-
-  //     // const downloadUrl = await response.text();
-  //     // window.location.href = downloadUrl;
-
-  //     // After successful upload, trigger the download on the client side
-  //     // const downloadLink = document.createElement('a');
-  //     // downloadLink.href = '/download';
-  //     // downloadLink.download = 'recorded_audio.webm';
-  //     // downloadLink.style.display = 'none';
-  //     // document.body.appendChild(downloadLink);
-  //     // downloadLink.click();
-  //     // document.body.removeChild(downloadLink);
-
-
-  //   } else {
-  //     console.error("Error uploading audio file: ", response.statusText);
-  //   }
-  //   } catch (error) {
-  //     console.error("Error uploading audio file:", error);
-  //   }
-    
-
-    // Optional: Clear the recorded audio chunks after upload
 
   document.querySelector('.redDot').style.display = 'none';
   // Disable the "Stop Recording" button
@@ -158,60 +112,6 @@ async function stopRecording() {
 
 
 }
-
-// function downloadAudio() {
-//   const latestAudio = audioChunks[audioChunks.length - 1];
-//   const downloadLink = document.createElement('a');
-//   downloadLink.href = latestAudio.url;
-//   downloadLink.download = 'recorded_audio.webm';
-//   downloadLink.style.display = 'none';
-//   document.body.appendChild(downloadLink);
-//   downloadLink.click();
-//   document.body.removeChild(downloadLink);
-// }
-
-
-
-// async function downloadAudio() {
-//   console.log('Download button clicked')
-//   try {
-//     const response = await fetch('/download');
-//     if (response.ok) {
-
-//       const link = document.createElement('a');
-//       link.href = '/download';
-//       link.download = 'recorded_audio.webm';
-//       link.style.display = 'none';
-
-//       // Add the link to the document and click it to initiate the download
-//       document.body.appendChild(link);
-//       link.click();
-
-//       // Remove the link from the document after the download
-//       document.body.removeChild(link);
-
-
-//       // window.location.href = '/download';
-
-//       // const blob = await response.blob();
-//       // const url = URL.createObjectURL(blob);
-//       // console.log(url);
-
-//       // Set the Blob URL as the href of the Download button
-//       // downloadButton.href = url;
-//       // downloadButton.download = 'recorded_audio.webm';
-
-//       // Simulate a click on the Download button to initiate the download
-//       // downloadButton.click();
-//     } else {
-//       console.error('Error fetching audio file:', response.statusText);
-//     }
-//   } catch (error) {
-//     console.error('Error fetching audio file:', error);
-//   }
-//   downloadButton.removeEventListener('click', downloadAudio);
-//   downloadEventAdded = false;
-// }
 
 async function downloadAudio() {
   console.log('Download button clicked');
@@ -240,31 +140,6 @@ async function downloadAudio() {
   }
   downloadButton.removeEventListener('click', downloadAudio);
   downloadEventAdded = false;
-
-  // try {
-  //   const response = await fetch('/download');
-  //   if (response.ok) {
-  //     const blob = await response.blob();
-
-  //     // Create a temporary link element to trigger the download
-  //     const url = URL.createObjectURL(blob);
-  //     const link = document.createElement('a');
-  //     link.href = url;
-  //     link.download = 'recorded_audio.webm';
-  //     link.style.display = 'none';
-
-  //     // Add the link to the document and click it to initiate the download
-  //     document.body.appendChild(link);
-  //     link.click();
-
-  //     // Remove the link from the document after the download
-  //     document.body.removeChild(link);
-  //   } else {
-  //     console.error('Error fetching audio file:', response.statusText);
-  //   }
-  // } catch (error) {
-  //   console.error('Error fetching audio file:', error);
-  // }
 }
 
 
@@ -285,27 +160,6 @@ async function playAudio() {
     // Play the audio
     audioElement.play();
 
-  // try {
-  //   const response = await fetch('/listen');
-  //   if (response.ok) {
-  //     const blob = await response.blob();
-  //     console.log(blob);
-  //     // Create a Blob URL from the received blob data
-  //     const audioURL = URL.createObjectURL(blob);
-  //     console.log(audioURL);
-
-
-  //     // Set the Blob URL as the src attribute of the audio element
-  //     audioPlayer.src = audioURL;
-
-  //     // Play the audio
-  //     audioPlayer.play();
-  //   } else {
-  //     console.error('Error fetching audio file:', response.statusText);
-  //   }
-  // } catch (error) {
-  //   console.error('Error fetching audio file:', error);
-  // }
 } catch (error) {
   console.error('Error fetching audio file:', error);
   }
